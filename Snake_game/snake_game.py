3549ce569851d0fde0f59d77c59c950efa33755b
ShlyonskiyDaniil <daniil.shlyonskiy@phystech.edu>
2019-04-09 21:58:26 +0300

    Snake_game

--- Snake_game/snake_game.py
+++ Snake_game/snake_game.py
@@ -10,48 +10,49 @@ from colours import Colours
 
 
 class Game():
-    player_wasd = 'Player1'
-    player_ijkl = 'Player2'
-    game_time = 60
+    def run(self):
+        player_wasd = 'Player1'
+        player_ijkl = 'Player2'
+        game_time = 60
 
-    screen_size = (520, 500)
+        screen_size = (520, 500)
 
-    pg.init()
-    screen = pg.display.set_mode(screen_size)
-    pg.display.set_caption("Snake forever")
-    delay = 50
+        pg.init()
+        screen = pg.display.set_mode(screen_size)
+        pg.display.set_caption("Snake forever")
+        delay = 50
 
-    first_snake = Snake(screen_size[1])
-    second_snake = Snake_second(screen_size[1])
-    control_first_snake = Control_first_snake()
-    control_second_snake = Control_second_snake()
-    first_food = Food(screen_size)
-    second_food = Food(screen_size)
-
-    game = True
-    while game:
         first_snake = Snake(screen_size[1])
         second_snake = Snake_second(screen_size[1])
         control_first_snake = Control_first_snake()
         control_second_snake = Control_second_snake()
         first_food = Food(screen_size)
         second_food = Food(screen_size)
-        time_flag = [True, ]
 
-        player_quantity = menu_screen(screen, screen_size, Colours().BLACK)
-        game_cycle(screen, screen_size, delay,
-                   first_snake, second_snake,
-                   control_first_snake,
-                   control_second_snake,
-                   first_food, second_food,
-                   game_time, time_flag,
-                   player_quantity)
-        if player_quantity == 'multi':
-            multi_result_screen(screen, first_snake,
-                                second_snake, first_food,
-                                second_food, player_wasd,
-                                player_ijkl, time_flag)
-        elif player_quantity == 'single':
-            single_result_screen(screen, first_snake,
-                                 first_food, player_wasd,
-                                 time_flag)
+        game = True
+        while game:
+            first_snake = Snake(screen_size[1])
+            second_snake = Snake_second(screen_size[1])
+            control_first_snake = Control_first_snake()
+            control_second_snake = Control_second_snake()
+            first_food = Food(screen_size)
+            second_food = Food(screen_size)
+            time_flag = [True, ]
+
+            player_quantity = menu_screen(screen, screen_size, Colours().BLACK)
+            game_cycle(screen, screen_size, delay,
+                       first_snake, second_snake,
+                       control_first_snake,
+                       control_second_snake,
+                       first_food, second_food,
+                       game_time, time_flag,
+                       player_quantity)
+            if player_quantity == 'multi':
+                multi_result_screen(screen, first_snake,
+                                    second_snake, first_food,
+                                    second_food, player_wasd,
+                                    player_ijkl, time_flag)
+            elif player_quantity == 'single':
+                single_result_screen(screen, first_snake,
+                                     first_food, player_wasd,
+                                     time_flag)
