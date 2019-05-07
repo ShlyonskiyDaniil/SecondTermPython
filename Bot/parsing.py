dfc82bdb17e5460942ccf4b54bf0264694e43ed3
ShlyonskiyDaniil <daniil.shlyonskiy@phystech.edu>
2019-05-07 23:05:53 +0300

    teleBot

---
+++ Bot/parsing.py
@@ -0,0 +1,76 @@
+import urllib.request as req
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+        response = req.urlopen(url)
+        return response.read()
+
+
+class Parser(object):
+    def __init__(self):
+        self.URL = 'https://yandex.ru/pogoda/region/225'
+        self.PREFIX = 'https://yandex.ru'
+
+    def towns_to_links(self, alpha_list, alpha):
+        town_link_part = alpha.find_all('a', class_="link link_theme_normal place-list__item-name i-bem")
+        for elem in town_link_part:
+            town = elem.text
+            link = self.PREFIX + elem['href']
+            alpha_list.append({town: link})
+
+    def parse_places(self, html):
+        all_cities = dict()
+
+        soup = BeautifulSoup(html)
+        main_page = soup.find('div', class_='content')
+        alpha_header = main_page.find_all('div', class_='place-list')
+
+        for alpha in alpha_header:
+            letter = alpha.find('h2', class_='title title_level_2 place-list__letter').text
+            if letter not in all_cities.keys():
+                all_cities[letter] = list()
+            
+            self.towns_to_links(all_cities[letter], alpha)
+
+        return all_cities
+
+    def possible_dates(self, weather_html):
+        data = dict()
+        soup = BeautifulSoup(weather_html)
+
+        main_page = soup.find('div', class_='forecast-briefly__days')
+
+        template_columns = main_page.find_all('a')
+
+        dates = list()
+        for data_part in template_columns:
+            dates.append(data_part.find('time', class_='time forecast-briefly__date').text)
+        
+        return dates
+
+    def parse_weather(self, date, weather_html):
+        data = dict()
+        soup = BeautifulSoup(weather_html)
+
+        main_page = soup.find('div', class_='forecast-briefly__days')
+
+        weather_columns = main_page.find_all('a')
+
+        dates = self.possible_dates(weather_html)
+        dates_counter = 0
+        for data_part in weather_columns:
+            day = data_part.find('div', class_='temp forecast-briefly__temp forecast-briefly__temp_day')
+            data[dates[dates_counter]] = [{'day': day.find('span', class_='temp__value').text}]
+
+            night = data_part.find('div', class_='temp forecast-briefly__temp forecast-briefly__temp_night')
+            data[dates[dates_counter]].append({'night': night.find('span', class_='temp__value').text})
+
+            weather_condition = data_part.find('div', class_='forecast-briefly__condition')
+            data[dates[dates_counter]].append({'condition': weather_condition.text})
+            dates_counter += 1
+
+        if date in dates:
+            return data[date]
+        else:
+            return data
---
+++ Bot/to_user.py
@@ -0,0 +1,86 @@
+from parsing import Parser, get_html
+
+
+def in_required(choosed, required):
+        for item in required:
+            if (choosed.upper() == list(item)[0].upper() or
+                 choosed.upper() in list(item)[0].upper()[:10]):
+                return True
+
+def place_to_link(places, required_place):
+    print(f'places is {places}')
+    print('#' * 20)
+    print(f'required is {required_place}')
+    for place in places:
+        if (required_place.upper() == list(place.keys())[0].upper()):
+            print(list(place.keys())[0])
+            return place[list(place.keys())[0]]
+        elif required_place.upper() in list(place.keys())[0].upper()[:10]:
+            print(list(place.keys())[0])
+            return place[list(place.keys())[0]]
+
+# #  Я хочу:
+# Получить букву - выдать регионы.
+# Получить название региона, получить первую букву города - выдать города.
+# Получить название города - выдать weather pattern. 
+class User_talk(object):
+    def __init__(self):
+        self.parser = Parser()
+        self.required_regions = None
+        self.required_towns = None
+        self.choosed_region = None
+        self.choosed_town = None
+
+    def letter_to_regions(self, region_letter):
+        all_regions = self.parser.parse_places(get_html(self.parser.URL))
+        self.required_regions = all_regions[region_letter]
+
+        for key in self.required_regions:
+            print(list(key.keys())[0])
+
+
+    def region_to_towns(self, choosed_region, town_letter):
+        region_url = str()
+
+        while not in_required(self.choosed_region, self.required_regions):
+            self.choosed_region = input('Choose your region: ')
+            if self.choosed_region == 'exit':
+                break
+
+        region_url = place_to_link(self.required_regions, self.choosed_region)
+        all_towns = self.parser.parse_places(get_html(region_url))
+
+        self.required_towns = all_towns[town_letter]
+        # print(required_towns)
+
+        for key in self.required_towns:
+            print(key.keys())
+        
+    def date_to_weather(self):
+        town_url = place_to_link(self.required_towns, self.choosed_town)
+        return self.parser.possible_dates(get_html(town_url))
+
+    def town_to_weather(self, date):
+        town_url = str()
+
+        while not in_required(self.choosed_town, self.required_towns):
+            self.choosed_town = input('Choose your town: ')
+            if self.choosed_town == 'exit':
+                break
+
+        town_url = place_to_link(self.required_towns, self.choosed_town)
+        return self.parser.parse_weather(date, get_html(town_url))
+
+
+
+# ut = User_talk()
+
+# region_letter = input('Write the first letter of you region: ')
+# ut.letter_to_regions(region_letter)
+
+# choosed_region = input('Choose your region: ')
+# town_letter = input('Write the first letter of your town: ')
+# ut.region_to_towns(choosed_region, town_letter)
+
+# choosed_town= input('Choose your town: ')
+# ut.town_to_weather(choosed_town)
